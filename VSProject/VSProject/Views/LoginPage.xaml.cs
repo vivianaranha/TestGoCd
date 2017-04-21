e1ae4630228d53653866e5e1540ca45278242e4a
Vivian Aranha <vivianaranha@Vivians-MacBook-Pro-2.local>
2017-04-20 21:12:15 -0400

    New Project

---
+++ VSProject/VSProject/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VSProject
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
